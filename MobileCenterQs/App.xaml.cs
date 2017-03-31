0049f5946db8d2d191fe85bc47397f4347afe264
Vu Duc Tuyen <tuyen@naxam.net>
2017-03-31 23:29:06 +0700

    Start to test

---
+++ MobileCenterQs/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+using Xamarin.Forms;
+
+namespace MobileCenterQs
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MobileCenterQsPage();
+        }
+
+        protected override void OnStart()
+        {
+			MobileCenter.Start(
+                    "android=0b90a986-7763-46b1-a7e9-21f19506220a;ios=1f1807e4-d38b-4888-98d3-5fd5921ab930",
+				    typeof(Analytics), 
+                    typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ MobileCenterQs/MobileCenterQsPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace MobileCenterQs
+{
+    public partial class MobileCenterQsPage : ContentPage
+    {
+        public MobileCenterQsPage()
+        {
+            InitializeComponent();
+
+			txtUserName.TextChanged += ValidateMandatory;
+            txtPassword.TextChanged += ValidateMandatory;
+
+            btnLogin.Clicked += DoLogin;
+        }
+
+        void DoLogin(object sender, EventArgs e)
+        {
+			var userName = txtUserName.Text;
+			var password = txtPassword.Text;
+
+            if (userName.Contains("ex")) {
+                throw new NotSupportedException("You have entered invalid data!");
+            }
+
+            DisplayAlert("Greeting", $"Welcome {userName} to Mobile Center!", "Ok");
+        }
+
+
+        void ValidateMandatory(object sender, TextChangedEventArgs e)
+        {
+			var userName = txtUserName.Text;
+			var password = txtPassword.Text;
+
+            var shouldEnable = !string.IsNullOrWhiteSpace(userName) && !string.IsNullOrWhiteSpace(password);
+
+            btnLogin.IsEnabled = shouldEnable;
+        }
+   }
+}
